a26a5e0645791a593695edbe3cccbdab0c21a61f
Ritik Jain <ritikjain1002@gmail.com>
2020-01-14 11:30:18 +0530

    Update Knight Path 2.cpp

--- Knight Path 2.cpp
+++ Knight Path 2.cpp
@@ -1,61 +1,38 @@
 #include<bits/stdc++.h>
+#define ll long long
 using namespace std;
 
-struct cell{
-    int x, y, d;
-    cell(){}
-    cell(int x, int y, int d) : x(x), y(y), d(d){}
-};
-
-int knightPath(int);
-bool isSafe(int, int, int);
-
 int main(){
-    int t, n;
+    ll x0, y0, x1, y1, n, r, a, b;
+    ll xmove[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+    ll ymove[] = {0, 1, 1, 1, 0, -1, -1, -1};
+    map<pair<ll, ll>, ll> om;
+    queue<pair<ll, ll>> q;
 
-    cin >> t;
-    while(t--){
-        cin >> n;
-        cout << knightPath(n) << endl;
+    cin >> x0 >> y0 >> x1 >> y1 >> n;
+    for(ll i = 0; i < n; i++){
+        cin >> r >> a >> b;
+        for(ll j = a; j <= b; j++)
+            om[{r, j}] = -1;
     }
-    return 0;
-}
-
-int knightPath(int n){
-    int kx, ky, tx, ty;
-    bool visited[n][n];
-    queue<cell> q;
-
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++)
-            visited[i][j] = false;
-    cin >> kx >> ky >> tx >> ty;
-    kx--;
-    ky--;
-    tx--;
-    ty--;
-    visited[kx][ky] = true;
-    q.push(cell(kx, ky, 0));
+    om[{x0, y0}] = 0;
+    q.push({x0, y0});
     while(!q.empty()){
-        cell temp = q.front();
+        pair<ll, ll> temp = q.front();
         q.pop();
-        if(temp.x == tx && temp.y == ty)
-            return temp.d;
-        int xmove[] = {-2, -2, -1, -1, 2, 2, 1, 1};
-        int ymove[] = {1, -1, 2, -2, 1, -1, 2, -2};
-        for(int i = 0; i < 8; i++){
-            int _x = temp.x + xmove[i];
-            int _y = temp.y + ymove[i];
-            if(isSafe(_x, _y, n) && visited[_x][_y] == false){
-                visited[_x][_y] = true;
-                q.push(cell(_x, _y, temp.d + 1));
+        if(temp.first == x1 && temp.second == y1){
+            cout << om[temp];
+            return 0;
+        }
+        for(ll i = 0; i < 8; i++){
+            ll _x = temp.first + xmove[i];
+            ll _y = temp.second + ymove[i];
+            if(om.find({_x, _y}) != om.end() && om[{_x, _y}] == -1){
+                om[{_x, _y}] = om[temp] + 1;
+                q.push({_x, _y});
             }
         }
     }
-}
-
-bool isSafe(int x, int y, int n){
-    if(0 <= x && x < n && 0 <= y && y < n)
-        return true;
-    return false;
+    cout << -1;
+    return 0;
 }
